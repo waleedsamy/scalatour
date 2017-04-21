f5c6f1b21b51cab31d328d63c9ce748f027a41bd
Waleed Samy <waleedsamy634@gmail.com>
2017-04-21 13:30:50 +0200

    Future with flatMap

--- src/main/scala/io/github/waleedsamy/scalatour/Futures/FutureWithFlatMap.scala
+++ src/main/scala/io/github/waleedsamy/scalatour/Futures/FutureWithFlatMap.scala
@@ -1,5 +1,28 @@
 package io.github.waleedsamy.scalatour.Futures
 
-object FutureWithFlatMap {
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{ Failure, Success }
+
+object FutureWithFlatMap extends App {
+
+  def isProfitable(x: Double, y: Double) = if (x > y) true else false
+
+  def buy(amount: Double, quota: Double): Future[Double] = Future { amount }
+
+  val usdQuote = Future { 3.1 }
+
+  val chfQuote = Future { 1.9 }
+
+  val purchase: Future[Double] = usdQuote.flatMap { usd =>
+    chfQuote.withFilter(chf => isProfitable(usd, chf)).flatMap {
+      chf => buy(4, chf)
+    }
+  }
+
+  purchase onComplete {
+    case Success(e) => println(s"$e is purchase")
+    case Failure(t) => println(s"$t")
+  }
 
 }
